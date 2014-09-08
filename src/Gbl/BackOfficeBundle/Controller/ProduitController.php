be39410ce06e068efb979268e9bb231905595f42
Chris <Administrateur@192.168.2.11>
2014-09-08 12:40:48 +0200

    Produit Controller

--- src/Gbl/BackOfficeBundle/Controller/ProduitController.php
+++ src/Gbl/BackOfficeBundle/Controller/ProduitController.php
@@ -6,21 +6,100 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Gbl\BackOfficeBundle\Entity\Produit;
+use Gbl\BackOfficeBundle\Form\ProduitType;
 
 class ProduitController extends Controller
 {
+/**
+	 * @Route("/produit/index", name="produit.index")
+	 */
 	public function indexAction()
 	{
+		$repository = $this->getDoctrine()->getManager()->getRepository('GblBackOfficeBundle:Produit');
+		$listeproduits = $repository->findAll();
 		
+		return $this->render('GblBackOfficeBundle:Produit:index.html.twig', array('produits' => $listeproduits));
 	}
 	
-	public function newAction()
+	/**
+	 * @Route("/produit/new", name="produit.new")
+	 * 
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function newAction(Request $request)
 	{
+		$prod = new produit();
+		$form = $this->createForm(new ProduitType(), $prod);
 		
+		$form->handleRequest($request);
+		
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($prod);
+			$em->flush();
+			$this->get('session')->getFlashBag()->add(
+                'notice',
+                'Produit enregistrée'
+            );
+			return $this->redirect($this->generateUrl('produit.index'));
+            
+        } else {
+            return $this->render('GblBackOfficeBundle:Produit:new.html.twig', array(
+                'form' => $form->createView(),
+            )); 
+        }
+	}
+		
+	/**
+	 * @Route("/produit/edit/{id}", name="produit.edit")
+	 */
+	public function editAction(Request $request, $id)
+	{
+		$prod = $this->getDoctrine()->getRepository('GblBackOfficeBundle:Produit')->find($id);
+			
+		if ($prod) {
+			$form = $this->createForm(new ProduitType(), $prod);
+		}
+		
+		$form->handleRequest($request);
+		
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($prod);
+			$em->flush();
+			$this->get('session')->getFlashBag()->add(
+				'notice',
+				'Produit modifiée'
+			);
+			return $this->redirect($this->generateUrl('produit.index'));
+				
+		} else {
+			return $this->render('GblBackOfficeBundle:Produit:new.html.twig', array(
+					'form' => $form->createView(),
+			));
+		}
 	}
 	
-	public function viewAction()
+	/**
+	 * @Route("/produit/delete/{id}", name="produit.delete")
+	 */
+	public function deleteAction($id)
 	{
+		$prod = $this->getDoctrine()->getRepository('GblBackOfficeBundle:Produit')->find($id);
 		
+		if ($prod) {
+			$em = $this->getDoctrine()->getManager();
+			$em->remove($prod);
+			$em->flush();
+			$this->get('session')->getFlashBag()->add(
+					'notice',
+					'Produit supprimée'
+			);
+			return $this->redirect($this->generateUrl('produit.index'));
+		} else {
+			$form = $this->createForm(new ProduitType(), $prod);
+			return $this->redirect($this->generateUrl('produit.index'));
+		}
 	}
 }
