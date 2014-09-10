76ad4daa00185ca4853fb0b8bc65f855305e539e
Antony <fullwz@gmail.com>
2014-09-10 10:38:35 +0200

    Formulaire de login magnifiquement beau

--- src/Gbl/BackOfficeBundle/Controller/ConfigurationController.php
+++ src/Gbl/BackOfficeBundle/Controller/ConfigurationController.php
@@ -5,12 +5,14 @@ namespace Gbl\BackOfficeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
-class ConfigurationController extends Controller 
+class ConfigurationController extends Controller
 {
 	/**
-	 * Route("/configuration", name="configuration.index")
-	 * Template()
+	 * @Route("/configuration", name="configuration.index")
+	 * @Template()
+	 * @Secure(roles="IS_AUTHENTICATED_REMEMBERED")
 	 */
 	public function indexAction()
 	{
